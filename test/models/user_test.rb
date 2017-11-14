2e75fd5b7110a0c1ba59fb70996a9d2e48176bde
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-14 15:19:18 +0900

    uniquness検証用コード追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -38,12 +38,19 @@ class UserTest < ActiveSupport::TestCase
    	    end
    	  end
 
-   	  test "email validation should reject invalid addresses" do
+   	test "email validation should reject invalid addresses" do
    	      invalid_addresses = %w[user@gmail,com user_at_foo.org user.name@example.
    	                             foo@bar_baz.com foo@bar+baz.com foo@bar..com]
    	      invalid_addresses.each do |invalid_address|
    	        @user.email = invalid_address
    	        assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
    	      end
-   	    end
+   	end
+
+   	test "email addresses should be unique" do
+   	    duplicate_user = @user.dup
+   	    duplicate_user.email = @user.email.upcase
+   	    @user.save
+   	    assert_not duplicate_user.valid?
+   	  end
 end
