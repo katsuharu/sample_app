d70ae10c74303939d786aa2870085998824945d0
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-14 15:06:39 +0900

    .が二回続く無効なメールアドレスを検証用に追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,7 +31,7 @@ class UserTest < ActiveSupport::TestCase
 
    	test "email validation should accept valid addresses" do
    	    valid_addresses = %w[user@gmail.com USER@foo.COM A_US-ER@foo.bar.org
-   	                         first.last@foo.jp alice+bob@baz.cn]
+   	                         first.last@foo.jp alice+bob@baz.cn foo@bar..com]
    	    valid_addresses.each do |valid_address|
    	      @user.email = valid_address
    	      assert @user.valid?, "#{valid_address.inspect} should be valid"
@@ -40,7 +40,7 @@ class UserTest < ActiveSupport::TestCase
 
    	  test "email validation should reject invalid addresses" do
    	      invalid_addresses = %w[user@gmail,com user_at_foo.org user.name@example.
-   	                             foo@bar_baz.com foo@bar+baz.com]
+   	                             foo@bar_baz.com foo@bar+baz.com foo@bar..com]
    	      invalid_addresses.each do |invalid_address|
    	        @user.email = invalid_address
    	        assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
