e2fcdd9591f02503d127fa8bfa5a369e688005b3
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-14 15:57:58 +0900

    パスワードをチェックするために関数を追加し、検証用のユーザーにパスワードを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 	def setup
-		@user = User.new(name: "Example User", email: "user@gmail.com")
+		@user = User.new(name: "Example User", email: "user@gmail.com",
+			password: "foobar", password_confirmation: "foobar")
 	end
 
 	test "should be valid" do
