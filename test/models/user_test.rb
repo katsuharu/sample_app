0ec362698aa8bbd20ac511183dab708e15b5972d
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-14 15:40:00 +0900

    メールアドレスを小文字にするテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -52,5 +52,12 @@ class UserTest < ActiveSupport::TestCase
    	    duplicate_user.email = @user.email.upcase
    	    @user.save
    	    assert_not duplicate_user.valid?
+   	end
+
+   	test "email addresses should be saved as lower-case" do
+   	    mixed_case_email = "Foo@ExAMPle.CoM"
+   	    @user.email = mixed_case_email
+   	    @user.save
+   	    assert_equal mixed_case_email.downcase, @user.reload.email
    	  end
 end
