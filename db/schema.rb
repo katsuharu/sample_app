ea86d8940500660248986ee4ecc83644e3bea012
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-14 15:56:48 +0900

    usersテーブルにpassword_digestを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114062356) do
+ActiveRecord::Schema.define(version: 20171114064912) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171114062356) do
     t.integer  "pair_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
