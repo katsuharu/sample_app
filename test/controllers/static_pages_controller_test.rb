36efeb13742142abc70e1af7da0e68756add4c7a
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-14 10:35:26 +0900

    インデント調整

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,10 +23,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 	assert_select "title", "About | #{@base_title}"
   end
 
-    test "should get contact" do
-    	get contact_path
-        assert_response :success
-        assert_select "title", "Contact | #{@base_title}"
-      end
+   end
 
 end
