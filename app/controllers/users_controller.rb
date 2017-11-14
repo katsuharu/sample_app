d055e164ec65ccb9efb78d8ec3c05b7735b51812
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-14 18:48:29 +0900

    ユーザー登録成功時にユーザページにリダイレクトして、メッセージを表示するように修正。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-
+  		flash[:success] = "ユーザー登録に成功いたしました。"
+  		redirect_to @user
   	else
   		render 'new'
   	end
