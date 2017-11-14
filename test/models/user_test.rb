ea3ed58a8f6f94b4b637efeb1b2d5988784d0ac5
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-14 15:07:09 +0900

    無効なアドレがエラーを起こすことを確認後に削除

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,7 +31,7 @@ class UserTest < ActiveSupport::TestCase
 
    	test "email validation should accept valid addresses" do
    	    valid_addresses = %w[user@gmail.com USER@foo.COM A_US-ER@foo.bar.org
-   	                         first.last@foo.jp alice+bob@baz.cn foo@bar..com]
+   	                         first.last@foo.jp alice+bob@baz.cn ]
    	    valid_addresses.each do |valid_address|
    	      @user.email = valid_address
    	      assert @user.valid?, "#{valid_address.inspect} should be valid"
