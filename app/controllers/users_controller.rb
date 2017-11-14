9ff259754b8c472104a4fb66b546e298921bdf8c
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-14 17:48:53 +0900

    ユーザー登録用ボタンが押されたときの関数を定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,18 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+
+  def create
+  	@user = User.new(user_ params)
+  	if @user.save
+
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation, :department_name, :slack_id)
 end
