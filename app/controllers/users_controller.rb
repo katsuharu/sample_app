68d5816302c96719888531c4cf03c835a3a369a5
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-15 14:04:48 +0900

    beforeフィルターを使い、ログインユーザー以外がedit,updateにアクセスしようとした場合にはルートにリダイレクトするようにする関数を定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
   	@user = User.find(params[:id])
@@ -45,5 +46,10 @@ class UsersController < ApplicationController
         flash[:danger] = "ログインしてください"
         redirect_to login_url
       end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 end
