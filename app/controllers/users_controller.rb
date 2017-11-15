19e9b81e66e10573115de70278c57afaa391db5d
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-15 13:59:58 +0900

    使用する関数名を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
 
     #ログイン済みユーザーかどうか確認
     def logged_in_user
-      unless logged_in_user?
+      unless logged_in?
         flash[:danger] = "ログインしてください"
         redirect_to login_url
       end
