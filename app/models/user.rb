f14366ef2026f1bd3a07a000d3d037a0954d414c
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-15 11:07:55 +0900

    永続セッションのためにDBにトークンを記憶する関数を定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
 	before_save {email.downcase!}
 	# before_save {self.email = self.email.downcase}
 	validates :name, presence: true, length: { maximum:50 }
@@ -7,4 +8,22 @@ class User < ApplicationRecord
 	uniqueness:{case_sensitive: false}
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
+
+	# 渡された文字列のハッシュ値を返す
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+		                                              BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
+
+	# ランダムなトークンを返す
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	#永続セッションのためにユーザーをDBに記憶する
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
 end
