0c62dd9bf1aec82ecbbf94392973f18998051706
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-15 14:55:03 +0900

    usersテーブルにadominカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115014153) do
+ActiveRecord::Schema.define(version: 20171115054731) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20171115014153) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
