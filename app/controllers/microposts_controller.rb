deae1e48dbcb9978f4d22ccd16b5d9b3c5c8273d
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-16 21:52:13 +0900

    webから画像を挿入できるように修正

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -17,7 +17,7 @@ class MicropostsController < ApplicationController
 	private
 
 		def micropost_params
-	    	params.require(:micropost).permit(:content)
+	    	params.require(:micropost).permit(:content, :picture)
 	    end
 
 end
