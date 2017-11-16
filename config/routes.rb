df27819b217d2c510ffd186b526aff7786efa3f5
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-16 12:09:23 +0900

    不要な行を削除
    gitの操作で混入した行を削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'sessions/new'
 
   get 'users/new'
@@ -11,19 +10,13 @@ Rails.application.routes.draw do
   get '/signup', to:'users#new'
   post '/signup',  to: 'users#create'
 
-=======
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
   get    '/signup',  to: 'users#new'
->>>>>>> a14ddf20322464f83cf514c0f6eb51c657f5bb00
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
-<<<<<<< HEAD
-end
-=======
-end
->>>>>>> a14ddf20322464f83cf514c0f6eb51c657f5bb00
