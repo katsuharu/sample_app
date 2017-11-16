3a62f096d3f580ad3860b9e97fa63dde65b03b1c
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-16 22:31:28 +0900

    空の配列を追加
    投稿が失敗した場合に備えて、空の配列を追加

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,10 +4,11 @@ class MicropostsController < ApplicationController
 	def create
 		@micropost = current_user.microposts.build(micropost_params)
 	    if @micropost.save
-	      flash[:success] = "Micropost created!"
-	      redirect_to root_url
+	    	flash[:success] = "Micropost created!"
+	    	redirect_to root_url
 	    else
-	      render 'static_pages/home'
+	    	@feed_items = []
+	     	render 'static_pages/home'
 	    end
 	end
 
