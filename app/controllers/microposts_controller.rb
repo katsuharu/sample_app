9183a391635ca1550f464637e7753db37e506b90
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-16 22:43:50 +0900

    投稿削除用のdestory関数を定義し、フィルターに追加

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
 	before_action :logged_in_user, only: [:create, :destroy]
-
+	before_action :correct_user,   only: :destroy
+	
 	def create
 		@micropost = current_user.microposts.build(micropost_params)
 	    if @micropost.save
@@ -13,6 +14,9 @@ class MicropostsController < ApplicationController
 	end
 
 	def destroy
+	    @micropost.destroy
+	    flash[:success] = "投稿が削除されました。"
+	    redirect_to request.referrer || root_url
 	end
 
 	private
