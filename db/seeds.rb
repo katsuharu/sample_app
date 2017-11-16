5544915e4cc33933f3916c95c2fd3485831493cb
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-16 20:16:29 +0900

    idが最小のユーザーにマイクロポストを3つ付与

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,12 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true)
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+
+users = User.order(:created_at).take(1)
+3.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
