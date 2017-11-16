39f99b5f7e01211eda1697f6c92d9be2842744ef
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-16 14:31:03 +0900

    アカウントを有効化するeditアクションを定義

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,15 @@
 class AccountActivationsController < ApplicationController
+	def edit
+	    user = User.find_by(email: params[:email])
+	    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+	      user.update_attribute(:activated,    true)
+	      user.update_attribute(:activated_at, Time.zone.now)
+	      log_in user
+	      flash[:success] = "アカウントが有効化されました。"
+	      redirect_to user
+	    else
+	      flash[:danger] = "このリンクは無効です。"
+	      redirect_to root_url
+	    end
+	end
 end
