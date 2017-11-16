46e2caac4ce172417eb962f20cb441c78442d88b
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-16 18:28:26 +0900

    パスワードリセット用のアクションを定義

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -21,4 +21,44 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  def update
+      if params[:user][:password].empty?
+        @user.errors.add(:password, :blank)
+        render 'edit'
+      elsif @user.update_attributes(user_params) 
+        log_in @user
+        flash[:success] = "Password has been reset."
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # 正しいユーザーかどうか確認する
+    def valid_user
+      unless (@user && @user.activated? &&
+               @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+
+    # トークンが期限切れかどうか確認する
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
 end
