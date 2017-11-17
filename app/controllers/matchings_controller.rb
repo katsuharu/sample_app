2662ed946efaa4c7f2e47c1bb313156f3867cbed
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-17 22:35:01 +0900

    未定義だった関数を定義

--- app/controllers/matchings_controller.rb
+++ app/controllers/matchings_controller.rb
@@ -19,5 +19,13 @@ class MatchingsController < ApplicationController
 	  		render action: 'show'
 	  	end
 	end
-	
+
+
+	private
+
+		def correct_user
+		  @user = User.find(params[:id])
+		  redirect_to(root_url) unless current_user?(@user)
+		end
+
 end
