30b5859dc67163966f0c2802e250f356954b5e10
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-17 09:38:05 +0900

    画像をリサイズするために画像アップローダーを修正する

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,7 +2,8 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [350, 350]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
