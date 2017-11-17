89a438b858b52f5879f0ee484edcca207f762ce8
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-17 13:30:37 +0900

    マイポを削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,3 @@ User.create!(name:  "小野まさき",
              password_confirmation: "masaki",
              admin: true,
              activated: true)
-
-
-users = User.order(:created_at).take(2)
-3.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
