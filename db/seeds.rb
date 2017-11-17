2e20133f0b5ad7373c50dde8a84112f61426a5ea
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-17 16:20:00 +0900

    新たにユーザーと属性を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,29 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
+             entry_id: 1,
              admin: true,
              activated: true)
 
 User.create!(name:  "小野まさき",
              email: "ono@gmail.com",
+             password:              "masaki",
+             password_confirmation: "masaki",
+             entry_id: 1,
+             admin: true,
+             activated: true)
+
+
+User.create!(name:  "東京タワー",
+             email: "tokyo@gmail.com",
+             password:              "masaki",
+             password_confirmation: "masaki",
+             admin: true,
+             entry_id: 1,
+             activated: true)
+
+User.create!(name:  "紅葉",
+             email: "koyo@gmail.com",
              password:              "masaki",
              password_confirmation: "masaki",
              admin: true,
