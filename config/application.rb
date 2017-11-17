fcad41579522d0d705af5588047b26e83404c017
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-17 16:44:08 +0900

    DBで使用される時刻を東京時間に設定

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module SampleApp
   class Application < Rails::Application
 
   	config.time_zone = 'Tokyo'
+  	config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
