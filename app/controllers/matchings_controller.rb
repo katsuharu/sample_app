071c7458b27059beda429f0e09b995dc814f4c8e
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-17 22:21:01 +0900

    shuffle_lunchのアプリを参考に、別のコントローラentryアクションを定義
    ルーティング、テンプレファイルも作成

---
+++ app/controllers/matchings_controller.rb
@@ -0,0 +1,23 @@
+class MatchingsController < ApplicationController
+	before_action :logged_in_user, :correct_user, only: [ :entry]
+	before_action :correct_user,   only: [:entry]
+	
+	@@entry_id = 0
+	@@pair_no = 0
+
+	def entry
+		@@entry_id += 1
+		p @@entry_id
+		User.where(id: current_user.id).update(entry_id: @@entry_id)
+	  	flash[:success] = "シャッフルランチにエントリーしました。"
+
+		if @@entry_id % 3 == 0
+			@@pair_no += 1
+			p @@pair_no
+			User.where(entry_id: @@entry_id-2 .. @@entry_id).update(pair_id: @@pair_no)
+
+	  		render action: 'show'
+	  	end
+	end
+	
+end
