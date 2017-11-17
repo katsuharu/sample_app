27ce96cb85cd85f3bf5ee012a2ac2742a530e25f
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-17 15:36:36 +0900

    flashとredirect_toを追加
    プロフィールの更新に成功した場合にメッセージを表示して、プロフィール画面へリダイレクトする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # 更新に成功した場合を扱う。
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to user_path
     else
       render 'edit'
     end
