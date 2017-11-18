2ac7e5dd3e58e247637da97a587adabc2ea98009
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-18 21:36:16 +0900

    wheneverからrake実行の検証用コード

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,33 +1,16 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+require File.expand_path(File.dirname(__FILE__) + "/environment")
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
 
-# Learn more: http://github.com/javan/whenever
-
-# 出力先のログファイル
 set :output, 'log/crontab.log'
+ENV['RAILS_ENV'] ||= 'development'
+set :environment, ENV['RAILS_ENV']
 
-# ジョブの実行環境定義
-set :environment, :production
 
-every '1 0 * * *' do
-  command "echo 'you can use raw cron syntax too'"
+# 1分毎に回す
+every 1.minute do
+  command "echo 'mossmossmossmossmossmoss'"
 end
 
-
-# Matching
+every 1.minute do
+	rake 'matching:sample'
+end
