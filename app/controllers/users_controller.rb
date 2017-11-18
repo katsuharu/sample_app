491f714dc613a5245afc18cd9c315933e3f2e194
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-19 02:24:19 +0900

    _pathから名前付きURLに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
     p @@entry_id
     User.where(id: current_user.id).update(entry_id: @@entry_id)
     flash[:success] = "シャッフルランチにエントリーしました。"
-    redirect_to waiting_path
+    redirect_to waiting_url
 
     if @@entry_id % 3 == 0
       @@pair_no += 1
