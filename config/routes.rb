719f092b0706988525f22bc589dd287f9fd16060
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-19 02:13:27 +0900

    waitingへリダイレクトするようにルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  get    '/',        to: 'users#entry'
+  get    '/waiting',        to: 'users#entry'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
