736297b7c2825c9b48ecb803b19c9317a8d2bea7
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-19 10:08:57 +0900

    /checkをルートに追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
   get    '/waiting', to: 'static_pages#waiting'
+  get    '/check', to: 'users#check'
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
