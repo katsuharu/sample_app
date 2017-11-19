a6739d994f40a2ab5fa0f8e451918bcf1dc71277
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-19 10:00:31 +0900

    新たにカラムを初期設定する

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
+             department_name: "第一営業本部",
              entry_id: 1,
              admin: true,
              activated: true)
@@ -10,6 +11,7 @@ User.create!(name:  "小野まさき",
              email: "ono@gmail.com",
              password:              "masaki",
              password_confirmation: "masaki",
+             department_name: "VR",
              entry_id: 1,
              admin: true,
              activated: true)
@@ -19,6 +21,7 @@ User.create!(name:  "東京タワー",
              email: "tokyo@gmail.com",
              password:              "masaki",
              password_confirmation: "masaki",
+             department_name: "営業"
              admin: true,
              entry_id: 1,
              activated: true)
@@ -27,6 +30,7 @@ User.create!(name:  "紅葉",
              email: "koyo@gmail.com",
              password:              "masaki",
              password_confirmation: "masaki",
+             department_name: "広報"
              admin: true,
              activated: true)
 
@@ -35,6 +39,7 @@ User.create!(name:  "おーいお茶",
              email: "ocha@gmail.com",
              password:              "masaki",
              password_confirmation: "masaki",
+             department_name: "ゲームエンジニア",
              admin: true,
              entry_id: 11,
              activated: true)
