23fd59711341ee2f1d0dede30f2c360ce1ac5a14
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-19 18:19:23 +0900

    不要な関数を削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user,     only: :destroy
-  before_action :set_user, only: [:entry] 
 
   @@entry_id = 0
   @@pair_id = 0
@@ -94,7 +93,4 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user.admin?
     end
 
-    def set_user
-      @user = current_user
-    end
 end
