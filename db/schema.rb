678003ad57d9d7f63df46d9b267103ed47ebe500
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-20 21:43:46 +0900

    usersテーブルにposition_idを追加
    rails g migration AddPositionidToUsersで生成されたファイル群

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117051047) do
+ActiveRecord::Schema.define(version: 20171120123456) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20171117051047) do
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
     t.string   "profile_img"
+    t.integer  "position_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
