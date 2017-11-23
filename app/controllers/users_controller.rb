fd262b672de6fae4a4887d02cb4428d093b11711
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-24 01:43:49 +0900

    category_idを取得するように追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,7 +67,7 @@ class UsersController < ApplicationController
 
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation,
-        :profile_img, :department_name, :position_id, :slack_id)
+        :profile_img, :department_name, :position_id, :slack_id, :category_id)
   	end
 
 
