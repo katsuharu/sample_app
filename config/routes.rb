6bc18a7f718d1737820bb74c34c33f7c606f0369
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-23 22:33:22 +0900

    ルーティング先をentryに変更

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   
   post   '/login',   to: 'sessions#create'
-  post    '/waiting',        to: 'users#entry'
+  post    '/entry',        to: 'users#entry'
   post   '/contact', to: 'static_pages#send_mail'
 
   delete '/logout',  to: 'sessions#destroy'
