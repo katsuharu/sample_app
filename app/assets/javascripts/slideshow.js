fc90ab6705f4a33482d1df93464a933e671de80f
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-11-24 19:17:42 +0900

    スライドショー用のjsを作成
    トップ画面のエントリーユーザー表示に使用

---
+++ app/assets/javascripts/slideshow.js
@@ -0,0 +1,38 @@
+////////////////////////////////////////
+//　SCRIPT
+////////////////////////////////////////
+$(function(){
+	var setElm = $('.slideShow'),
+	fadeSpeed = 1000,
+	switchDelay = 2000;
+
+	$(window).load(function(){
+		setElm.each(function(){
+			var self = $(this),
+			findUl = self.find('ul'),
+			findLi = findUl.find('li'),
+			findLiFirst = findUl.find('li:first'),
+			findImg = findLi.find('img');
+
+			findLi.css({display:'block',opacity:'0',zIndex:'99'});
+
+			function areaHeight(){
+				var imgHeight = findImg.height();
+
+				self.css({height:imgHeight});
+				findUl.css({height:imgHeight});
+				findLi.css({height:imgHeight});
+			}
+			$(window).on('resize', function(){areaHeight();});
+			areaHeight();
+
+			findLiFirst.css({zIndex:'100'}).stop().animate({opacity:'1'},fadeSpeed);
+
+			setInterval(function(){
+				findUl.find('li:first').animate({opacity:'0'},
+					fadeSpeed).next('li').css({zIndex:'100'}).animate({opacity:'1'},fadeSpeed).end().appendTo(findUl).css({zIndex:'99'});
+			},switchDelay);
+
+		});
+	});
+});
