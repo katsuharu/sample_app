04447cb809cc488f7b26915973b90393300fcc30
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-12-08 16:09:00 +0900

    moduleをinclude
    データURIスキームを処理するためのモジュール

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	include DataUriParseable
+	
 	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save :downcase_email
 	# before_create :create_activation_digest
