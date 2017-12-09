88dfc36b0a24db8db3200ee577a36a8b4927aa60
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-12-09 13:36:43 +0900

    エントリーした内容を確認するページにいつでもアクセスできるよに、rootとcontrollerを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -93,6 +93,9 @@ class UsersController < ApplicationController
       
     end
   end
+
+  def check_entry_cnt
+  end
   
   def check
     @pairs = User.where(pair_id: current_user.pair_id).where.not(pair_id: nil).where(category_id: current_user.category_id).where.not(id:current_user.id)
