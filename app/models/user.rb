63a7cbddb5d7341191b2f267ed08ae5e0c8f0bf3
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-12-12 15:29:56 +0900

    validatesを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
 	mount_uploader :profile_img, PictureUploader
-	validates :profile_img, presence: true
+	# validates :profile_img, presence: true
 	validates :self_intro, presence: true, length: { maximum: 25 }
 	validates :department_name, presence: true
 	validates :slack_id, presence: true
