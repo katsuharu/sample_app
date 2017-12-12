953ad3b7e388bc96e98c744e99957144e58ca8da
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-12-12 11:43:26 +0900

    デバッグ用に関数を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,6 +85,7 @@ class User < ApplicationRecord
 	end
 
 	def set_avatar_from_data_uri
+		p "運慶"
 		logger.debug(avatar_data_uri)
 	    self.profile_img = self.class.data_uri_to_file(avatar_data_uri)
 	end
