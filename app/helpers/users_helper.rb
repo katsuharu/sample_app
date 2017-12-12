44c92af3d65c37f12cdc3e05dfaedacc989cca17
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-12-12 09:56:52 +0900

    mouduleで囲うのを解除

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,31 +7,29 @@ module UsersHelper extend ActiveSupport::Concern
 		!current_user.pair_id.nil?
 	end
 
-	module ClassMethods
-	  def data_uri_to_file data_uri
-	    data = data_uri.try do |uri|
-	      uri.match(%r{\Adata:(?<type>.*?);(?<encoder>.*?),(?<data>.*)\z}) do |md|
-	        {
-	          type:      md[:type],
-	          encoder:   md[:encoder],
-	          data:      Base64.decode64(md[:data]),
-	          extension: md[:type].split('/')[1]
-	        }
-	      end
-	    end
-	    return nil unless data
-	
-	    temp_file = Tempfile.new('uploaded-data_uri').tap do |file|
-	      file.binmode
-	      file << data[:data]
-	      file.rewind
-	    end
-	
-	    ActionDispatch::Http::UploadedFile.new(
-	      filename: "data_uri.#{data[:extension]}",
-	      type:     data[:type],
-	      tempfile: temp_file
-	    )
-	  end
+	def data_uri_to_file data_uri
+		data = data_uri.try do |uri|
+		  uri.match(%r{\Adata:(?<type>.*?);(?<encoder>.*?),(?<data>.*)\z}) do |md|
+		    {
+		      type:      md[:type],
+		      encoder:   md[:encoder],
+		      data:      Base64.decode64(md[:data]),
+		      extension: md[:type].split('/')[1]
+		    }
+		  end
+		end
+		return nil unless data
+
+		temp_file = Tempfile.new('uploaded-data_uri').tap do |file|
+		  file.binmode
+		  file << data[:data]
+		  file.rewind
+		end
+
+		ActionDispatch::Http::UploadedFile.new(
+		  filename: "data_uri.#{data[:extension]}",
+		  type:     data[:type],
+		  tempfile: temp_file
+		)
 	end
 end
