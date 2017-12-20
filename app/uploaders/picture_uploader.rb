5de5beb94bf65aea3629332d42603cd686c54a35
katsuharu <sasaki-katsuharu@kcgrp.jp>
2017-12-20 19:01:46 +0900

    関数を定義して、確認画面でアップロードされる画像のpathを変更
    defaultのpathに設定

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -15,6 +15,10 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def cache_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
