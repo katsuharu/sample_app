e0edaa08466df26ee5215e078f1de8a75430da4d
katsuharu <traveler.18.challenge@gmail.com>
2017-12-25 10:59:48 +0900

    authorizationモデルの生成
    amniauthを利用するために、provider(sns名),uid(ユーザーID)そして、userモデルと紐付けるためのuser_idカラムを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171220105016) do
+ActiveRecord::Schema.define(version: 20171225015502) do
+
+  create_table "authorizations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
