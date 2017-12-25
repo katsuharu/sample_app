0345b7b6cbe27ca390229084f79f490ad2a9404f
katsuharu <traveler.18.challenge@gmail.com>
2017-12-25 11:24:39 +0900

    ユーザー検索機能と新規に認証を追加する機能を追加

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -4,4 +4,14 @@ class Authorization < ApplicationRecord
 	validates_presence_of :user_id, :uid, :provider
 	#:providerと:uidのペアは一意であることを保証
 	validates_uniqueness_of :uid, uniqueness: {:scope => :provider}
+
+
+	def Authorization.find_from_auth(auth)
+	    find_by_provider_and_uid(auth['provider'], auth['uid'])
+	end
+	  
+	def Authorization.create_from_auth(auth, user = nil)
+	    user ||= User.create_from_auth!(auth)
+	    Authorization.create!(:user => user, :uid => auth['uid'], :provider => auth['provider'])
+	end
 end
