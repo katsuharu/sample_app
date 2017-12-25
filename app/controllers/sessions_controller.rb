428682dd7484103889461fe23bc6ac2f9ab5fc4e
katsuharu <traveler.18.challenge@gmail.com>
2017-12-25 11:36:29 +0900

    fb loginにseession機能を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,22 +3,31 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = User.find_by(email:params[:session][:email].downcase)
-  	if user && user.authenticate(params[:session][:password])
-  		if user.activated?
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  		redirect_back_or user
-      else
-        message = "アカウントが有効化されておりません。"
-        message += "送られてきたメールのリンクでアカウントを有効化してください。"
-        flash[:warning] = message
-        redirect_to root_url
+    auth = request.env["omniauth.auth"]
+    if auth.present?
+      unless @auth = Authorization.find_from_auth(auth)
+        @auth = Authorization.create_from_auth(auth)
       end
-  	else
-  		flash.now[:danger] = 'メールアドレスまたはパスワードが正しくありません。'
-    	render 'new'
-  	end
+      user = @auth.user
+      redirect_back_or user
+    else
+    	user = User.find_by(email:params[:session][:email].downcase)
+    	if user && user.authenticate(params[:session][:password])
+    		if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+    		redirect_back_or user
+        else
+          message = "アカウントが有効化されておりません。"
+          message += "送られてきたメールのリンクでアカウントを有効化してください。"
+          flash[:warning] = message
+          redirect_to root_url
+        end
+    	else
+    		flash.now[:danger] = 'メールアドレスまたはパスワードが正しくありません。'
+      	render 'new'
+    	end
+    end
   end
 
   def destroy
