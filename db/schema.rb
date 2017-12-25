d57cfeb07b95337cf49df5be33ddb5ef83cec6a4
katsuharu <traveler.18.challenge@gmail.com>
2017-12-25 11:02:05 +0900

    重複して認証しないようにindexを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171225015502) do
+ActiveRecord::Schema.define(version: 20171225015708) do
 
   create_table "authorizations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "provider"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171225015502) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_authorizations_on_provider_and_uid", unique: true, using: :btree
   end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
