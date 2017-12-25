e2ed20356205c35eac4cb5cbf41679ec92813cd1
katsuharu <traveler.18.challenge@gmail.com>
2017-12-25 11:07:13 +0900

    authorizastionモデルをuserモデルに紐付け、validationを追加

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,2 +1,7 @@
 class Authorization < ApplicationRecord
+	belongs_to :user
+	#:providerと:uidはもちろんのことユーザーとの紐付け(:user_id)も保証
+	validates_presence_of :user_id, :uid, :provider
+	#:providerと:uidのペアは一意であることを保証
+	validates_uniqueness_of :uid, uniqueness: {:scope => :provider}
 end
