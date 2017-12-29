3c2975708214d634c113c134894ba9b6df7f51ed
katsuharu <traveler.18.challenge@gmail.com>
2017-12-29 10:33:16 +0900

    secret keyを直接指定

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+	provider :facebook, "529254484110388", "71b7a37eb2fd4a1d69266fae83e9857d"
 end
