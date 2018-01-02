5a434955685a4fce1db3bc8ee069565368e274fd
katsuharu <traveler.18.challenge@gmail.com>
2018-01-02 16:47:18 +0900

    validatesの記述方法を変更

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,7 +1,10 @@
 class Authorization < ApplicationRecord
 	belongs_to :user
 	#:providerと:uidはもちろんのことユーザーとの紐付け(:user_id)も保証
-	validates_presence_of :user_id, :uid, :provider
+	validates :user_id, presence: true
+	validates :uid, presence: true
+	validates :provider, presence: true
+
 	#:providerと:uidのペアは一意であることを保証
 	validates_uniqueness_of :uid, uniqueness: {:scope => :provider}
 
