564a7d1822e16e5180e1ba34057dc3cdefddf312
katsuharu <traveler.18.challenge@gmail.com>
2018-01-02 12:03:11 +0900

    paginate関数の利用を廃止
    gemでpaginateをuninstallしたのに伴い利用を中止

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
   	if logged_in?
-      @users = User.where.not(category_id: nil).paginate(page: params[:page])
+      @users = User.where.not(category_id: nil)
     end
   end
 
