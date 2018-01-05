e8e9a8dd41796fd81b4129834e6c68c680176d26
katsuharu <traveler.18.challenge@gmail.com>
2018-01-05 13:28:32 +0900

    テストコードを変更

--- test/models/authorization_test.rb
+++ test/models/authorization_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class AuthorizationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+	   @user = users(:michael)
+	   # このコードは慣習的に正しくない
+	   @authorization = Authorization.new(content: "Lorem ipsum", user_id: @user.id)
+	 end
+
+	 test "should be valid" do
+	   assert @authorization.valid?
+	 end
+
+	 test "user id should be present" do
+	   @micropost.user_id = nil
+	   assert_not @micropost.valid?
+	 end
 end
