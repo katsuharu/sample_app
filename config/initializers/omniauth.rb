cb79acf498d4d470a547b061b21f812a2222f22a
katsuharu <traveler.18.challenge@gmail.com>
2018-01-09 10:03:09 +0900

    取得する情報(scop), libのversionなどを追記

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+	scope: 'email,user_birthday,user_likes, user_friends, public_profile', display: 'popup',
+	client_options: {
+	site: 'https://graph.facebook.com/v2.11',
+	authorize_url: "https://www.facebook.com/v2.11/dialog/oauth"
+	},
+	local: "ja_JP",
+	info_fields: "email, favorite_teams, user_birthday, favorite_athletes, gender, first_name, last_name"
 end
