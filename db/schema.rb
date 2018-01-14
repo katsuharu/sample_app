5d13f8ad4e19cca39344f7b1c139b0807650c931
katsuharu <traveler.18.challenge@gmail.com>
2018-01-14 15:29:35 +0900

    Userテーブルにログイン日時を記録するためのカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171228082347) do
+ActiveRecord::Schema.define(version: 20180113055805) do
 
   create_table "authorizations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "provider"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20171228082347) do
     t.integer  "any_category"
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
+    t.datetime "logined_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
