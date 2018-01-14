a1f62ad9dca3562b38439f2a6a2411a5b55f8179
katsuharu <traveler.18.challenge@gmail.com>
2018-01-14 18:59:58 +0900

    ログイン時のみcheck関数を行うに変更
    セキュリティやプライバシーの問題のため

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:show, :edit, :update, :destroy, :entry, :cancel]
+  before_action :logged_in_user, only: [:show, :edit, :update, :destroy, :entry, :cancel, :check]
   before_action :correct_user, only: [:show, :edit, :update]
   before_action :admin_user,     only: :destroy
 
