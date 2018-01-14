7f9390c71f3913158da1353593356f1fddd76297
katsuharu <traveler.18.challenge@gmail.com>
2018-01-14 19:21:26 +0900

    ユーザーがログインした場合にlogined_atを現在の時刻に更新するように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
 
   	#ログイン済みユーザーかどうか確認
   	def logged_in_user
-  	  unless logged_in?
+      if logged_in?
+        current_user.update_attribute(:logined_at, DateTime.now)
+      else
   	    store_location
   	    flash[:danger] = "ログインしてください"
   	    redirect_to login_url
