7f53f2f839c9fe55768f35b5ebdc6291d216bd1b
katsuharu <traveler.18.challenge@gmail.com>
2018-01-25 11:47:35 +0900

    UserHobbyモデルを正しい名前で作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125015123) do
+ActiveRecord::Schema.define(version: 20180125023204) do
 
   create_table "authorizations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "provider"
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 20180125015123) do
   create_table "user_hobbies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
     t.integer  "hobby_id"
+    t.string   "hobby_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "hobby_name"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
