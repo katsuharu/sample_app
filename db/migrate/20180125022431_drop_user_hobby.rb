9d2a29c99d15492436f14d63099c87cc305359cb
katsuharu <traveler.18.challenge@gmail.com>
2018-01-25 11:47:35 +0900

    UserHobbyモデルを正しい名前で作成

--- db/migrate/20180125022431_drop_user_hobby.rb
+++ db/migrate/20180125022431_drop_user_hobby.rb
@@ -1,5 +1,5 @@
 class DropUserHobby < ActiveRecord::Migration[5.0]
   def change
-  	drop_table :user_hobby
+  	drop_table :user_hobbies
   end
 end
---
+++ db/migrate/20180125023204_create_user_hobbies.rb
@@ -0,0 +1,11 @@
+class CreateUserHobbies < ActiveRecord::Migration[5.0]
+  def change
+    create_table :user_hobbies do |t|
+      t.integer :user_id
+      t.integer :hobby_id
+      t.string :hobby_name
+
+      t.timestamps
+    end
+  end
+end
