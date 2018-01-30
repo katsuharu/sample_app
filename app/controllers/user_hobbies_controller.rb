480c4d24a512a0ff59d75f686e9a11d320141112
katsuharu <traveler.18.challenge@gmail.com>
2018-01-30 15:51:17 +0900

    checkboxが何もcheckされていない場合にnilでエラーが発生してしまうのを防ぐために修正

--- app/controllers/user_hobbies_controller.rb
+++ app/controllers/user_hobbies_controller.rb
@@ -19,9 +19,17 @@ class UserHobbiesController < ApplicationController
 		#すでに登録されている趣味項目が削除ボタンを押された場合、アラートで確認後、実際に削除する
 		#新規にこの時登録するとき、 user_hobbiesテーブルのレコードに、今登録していようとしている趣味のuser_idとhobby_idが同一のレコードは既に
 		#登録されているとみなして登録しない。
-		del_num = params[:del_hobbies].count()
-		for i in 0..del_num - 1
-			UserHobby.where(user_id: current_user.id).where(hobby_name: params[:del_hobbies][i]).destroy_all
+		# add_num = params[:user_hobby][:hobby_name].count()
+		p params[:user_hobby]
+		# p add_num
+
+		del_hobbies = params[:del_hobbies]
+		if del_hobbies
+		
+			for str in del_hobbies
+				puts str
+				UserHobby.where(user_id: current_user.id).where(hobby_name: str).destroy_all
+			end
 		end
 	end
 
