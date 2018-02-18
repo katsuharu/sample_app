a3226dfc62b221d40e9ff7858afe6abb7a6ded51
katsuharu <traveler.18.challenge@gmail.com>
2018-02-18 16:20:19 +0900

    formからの値でtweetレコードを作成する

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,20 @@
 class TweetsController < ApplicationController
 	def create
+
+		if Tweet.create(content: tweet_params[:content], user_id: current_user.id)
+			p "Git tatekawa"
+			# format.html
+			# format.js
+		else
+			p "Oos"
+			# format.js {render :new}
+		end
 	end
+
+
+	private
+
+		def tweet_params
+      		params.require(:tweet).permit(:content)
+    	end
 end
