f0b15b047762126db99a96d05210799ac8f9cffa
katsuharu <traveler.18.challenge@gmail.com>
2018-02-24 19:05:04 +0900

    ajaxから送られてくる検索窓の文字を使ってレコード検索する

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,10 @@
 class CategoriesController < ApplicationController
-  def search
-  end
+    def search
+    	if params[:keyword] != ''
+	  	  	@categories = Category.where('name LIKE(?)', "#{params[:keyword]}%").or(Category.where('hira LIKE(?)', "#{params[:keyword]}%")).or(Category.where('kana LIKE(?)', "#{params[:keyword]}%"))
+	  	  	respond_to do |format|
+	  	  		format.json { render 'index', json: @categories }
+	  	  	end
+	  	end
+  	end
 end
