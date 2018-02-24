22a687f9840c149b420d0db75c86f4da2112b049
katsuharu <traveler.18.challenge@gmail.com>
2018-02-24 18:54:41 +0900

    controller生成時に不要なfileを生成しないように設定を変更

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,11 @@ module SampleApp
   	config.time_zone = 'Tokyo'
   	config.active_record.default_timezone = :local
   	config.i18n.default_locale = :ja
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+  	config.generators do |g|
+		g.javascripts false
+		g.stylesheets false
+		g.helper false
+		g.test_framework false
+	end
   end
 end
