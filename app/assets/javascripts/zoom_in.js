b0f25d402fa2e17933b9b0574f589ac859ded269
katsuharu <traveler.18.challenge@gmail.com>
2018-03-21 21:57:57 +0900

    第一階層の趣味をクリックした時に、そのカテゴリーが持つ下層のカテゴリーをズームインで表示するjsファイルを作成

---
+++ app/assets/javascripts/zoom_in.js
@@ -0,0 +1,34 @@
+$(document).ready(function (){
+
+  'use strict';
+
+  // 第一層のカテゴリーを選択した時に、その下層のカテゴリーを表示する
+  var $firsts = $('.filter [data-first]'),
+    $seconds = $('.second_categories [data-first-id]');
+  $firsts.on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    
+    $firsts.removeClass('active');
+    $this.addClass('active');
+
+    var $firstChild = $this.attr('data-first');
+
+    if ($firstChild == 'all') {
+      $seconds.removeClass('is-animated')
+        .fadeOut().promise().done(function() {
+          $seconds.addClass('is-animated').fadeIn();
+        });
+    } else {
+      $seconds.removeClass('is-animated')
+        .fadeOut().promise().done(function() {
+          $seconds.filter('[data-first-id = "' + $firstChild + '"]')
+            .addClass('is-animated').fadeIn();
+        });
+    }
+  })
+  //
+
+  
+
+})
