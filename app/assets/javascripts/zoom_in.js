d2fb39d81105725a3261e1319c8c818ea845f89b
katsuharu <traveler.18.challenge@gmail.com>
2018-03-24 11:01:16 +0900

    firstchildのattrの結果による条件分岐を削除/セミコロンを削除
    不要なため

--- app/assets/javascripts/zoom_in.js
+++ app/assets/javascripts/zoom_in.js
@@ -1,31 +1,24 @@
 $(document).ready(function (){
 
-  'use strict';
+  'use strict'
 
   // 第一層のカテゴリーを選択した時に、その下層のカテゴリーを表示する
   var $firsts = $('.first_categories [data-first]'),
-    $first_parents = $('.second_categories [data-first-id]');
+    $first_parents = $('.second_categories [data-first-id]')
   $firsts.on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
+    e.preventDefault()
+    var $this = $(this)
     
-    $firsts.removeClass('active');
-    $this.addClass('active');
+    $firsts.removeClass('active')
+    $this.addClass('active')
 
-    var $firstChild = $this.attr('data-first');
+    var $firstChild = $this.attr('data-first')
 
-    if ($firstChild == 'all') {
-      $first_parents.removeClass('is-animated')
-        .fadeOut().promise().done(function() {
-          $first_parents.addClass('is-animated').fadeIn();
-        });
-    } else {
-      $first_parents.removeClass('is-animated')
-        .fadeOut().promise().done(function() {
-          $first_parents.filter('[data-first-id = "' + $firstChild + '"]')
-            .addClass('is-animated').fadeIn();
-        });
-    }
+    $first_parents.removeClass('is-animated')
+      .fadeOut().promise().done(function() {
+        $first_parents.filter('[data-first-id = "' + $firstChild + '"]')
+          .addClass('is-animated').fadeIn();
+      })
   })
   //
 
