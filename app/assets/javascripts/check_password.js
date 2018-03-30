63fb15fd2a7eb389222c472ddb71886b8b01581c
katsuharu <traveler.18.challenge@gmail.com>
2018-03-30 23:17:01 +0900

    新規登録フォームでキャッシュとして画像が残っている場合はフロントの画像のバリデーションを解除する

--- app/assets/javascripts/check_password.js
+++ app/assets/javascripts/check_password.js
@@ -1,4 +1,11 @@
 $(document).ready(function (){
+	// 以前にアップロードした画像が存在する場合
+	if($('.vanish').attr('src')) {
+		$('#user_profile_img').removeAttr("required")
+	} else {
+		console.log('NONOJO')
+	}
+
 	$(document).on('click', '#submitBtn', function() {
 		if ($('#user_password').val() != $('#user_password_confirmation').val()) {
 			alert('パスワードと確認用パスワードが一致しません。')
