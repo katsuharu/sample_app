57bde9d362665ae39722d35aac76d6843661ab15
katsuharu <traveler.18.challenge@gmail.com>
2018-04-02 21:19:44 +0900

    投稿したあとに動的に投稿を表示する際にも降順で表示するようにDBから取得

--- app/controllers/apples_controller.rb
+++ app/controllers/apples_controller.rb
@@ -1,6 +1,6 @@
 class ApplesController < ApplicationController
 	def create
-		@apples = Tweet.all
+		@apples = Tweet.all.order('created_at DESC')
 		@apple = Tweet.new(apple_params)
 		respond_to do |format|
 			if Tweet.create(content: apple_params[:content], user_id: current_user.id, category_id: apple_params[:category_id])
