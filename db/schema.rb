43bd6cb9892f5c6e86486fc7bef75ab5580dbfd5
katsuharu <traveler.18.challenge@gmail.com>
2018-04-22 11:25:20 +0900

    [modify]lunchテーブルを作成
    エントリーとマッチング情報を記録

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322122034) do
+ActiveRecord::Schema.define(version: 20180422015728) do
 
   create_table "authorizations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "provider"
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(version: 20180322122034) do
     t.integer  "third_category_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+  end
+
+  create_table "lunches", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.integer  "pair_id"
+    t.boolean  "is_deleted"
+    t.datetime "deleted_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "positions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
