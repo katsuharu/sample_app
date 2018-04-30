5b9606f27a0fd8bfb580dbd66c8530332f760138
katsuharu <traveler.18.challenge@gmail.com>
2018-04-30 11:10:52 +0900

    [remove]capistrano migrateで エラー原因のmigrationファイルを削除

--- db/migrate/20180321083437_create_first_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateFirstCategories < ActiveRecord::Migration[5.0]
-  def change
-    create_table :first_categories do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20180322121434_drop_third_category.rb
+++
@@ -1,5 +0,0 @@
-class DropThirdCategory < ActiveRecord::Migration[5.0]
-  def change
-  	drop_table :third_categories
-  end
-end
