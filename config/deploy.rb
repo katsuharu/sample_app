f0e7b7c4ddd52f3b21df2a40f93adcde8e5e5640
katsuharu <traveler.18.challenge@gmail.com>
2018-06-17 02:41:07 +0900

    [modify]タスクを追加してタスク名を変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,11 +39,12 @@ set :keep_releases, 5
 
 namespace :deploy do
 
-  desc 'db_seed'
-  task :db_seed do
+  desc 'rails db migrate reset & db seed'
+  task :migrate_reset do
     on roles(:db) do |host|
       with rails_env: fetch(:rails_env) do
         within current_path do
+          execute :bundle, :exec, :rake, 'db:migrate:reset'
           execute :bundle, :exec, :rake, 'db:seed'
         end
       end
