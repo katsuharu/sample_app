20f610e7b4ec54d91cea107ea7b7ffeb64529d57
katsuharu <traveler.18.challenge@gmail.com>
2018-06-22 01:26:05 +0900

    Userモデルのpasswordカラムの指定方法を変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,10 +21,9 @@ CSV.foreach('db/category_new.csv') do |info|
 end
 
 CSV.foreach('db/users_seeds.csv') do |info|
-  User.create(id: info[0], name: info[1], email: info[2], password: User.digest(info[3]), 
-    	department_name: info[4], slack_id: info[5], activated: info[6], activated_at: info[7])
+  User.create(id: info[0], name: info[1], email: info[2], password: info[3], password_confirmation: info[3], department_name: info[4], slack_id: info[5], activated: info[6], activated_at: info[7])
 end
 
 CSV.foreach('db/lunch_seeds.csv') do |info|
-  Lunch.create(user_id: info[0], lunch_date: info[1], category_id: info[2])
+  Lunch.create(user_id: info[0], lunch_date: Date.today, category_id: info[2])
 end
