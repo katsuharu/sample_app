b5bff53e15422c2ac6c5805bccfb22c7804325c6
katsuharu <traveler.18.challenge@gmail.com>
2018-06-23 22:29:08 +0900

    [modify]バッチのスケジュールファイルを修正
    本番環境でも起動するように環境変数の設定を追加しスケジュールの時間を変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,12 @@
+## 空だった場合本番として実行
+ENV['RAILS_ENV'] ||= 'production'
+
 require File.expand_path(File.dirname(__FILE__) + "/environment")
-
 set :output, 'log/crontab.log'
-ENV['RAILS_ENV'] ||= 'development'
-set :environment, ENV['RAILS_ENV']
+## 環境ごとに切り合わけ
+set :environment, ENV['RAILS_ENV'] 
 
-
-every 1.minute do
-	rake 'matching:execute'
+# 平日の12時30分にスケジューリング
+every :weekday, at: '12:30 pm' do
+	rake 'matching:matching'
 end
