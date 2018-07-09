adfd1b9dbd9e0c7e1a8887f2b66cfec808719447
katsuharu <traveler.18.challenge@gmail.com>
2018-07-10 00:05:09 +0900

    [modify]pngファイルの画像がpreview表示されるように修正

--- app/assets/javascripts/img_change.js
+++ app/assets/javascripts/img_change.js
@@ -1,7 +1,23 @@
 $(document).on('turbolinks:load', function() {
-    $('#user_profile_img').on("change", function() {
-    	console.log("kkk"); // ファイル名をログに出力する
-    	$('.vanish').css('display', 'none');
-       }
-    )
-});
+  $('#user_profile_img').change(function(e){
+    $('.vanish').css('display', 'none')
+    //ファイルオブジェクトを取得する
+    var file = e.target.files[0]
+    var reader = new FileReader()
+
+    //画像でない場合は処理終了
+    if(file.type.indexOf("image") < 0){
+      alert("画像ファイルを指定してください。")
+      return false
+    }
+
+    //アップロードした画像を設定する
+    reader.onload = (function(file){
+      return function(e){
+        $(".cropit-preview-image").attr("src", e.target.result)
+        $(".cropit-preview-image").attr("title", file.name)
+      };
+    })(file);
+    reader.readAsDataURL(file)
+  })  
+})
