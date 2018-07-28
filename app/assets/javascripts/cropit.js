4b8f051498d7974849a9efa6ac5a5e5f41c561d0
katsuharu <traveler.18.challenge@gmail.com>
2018-07-28 14:49:12 +0900

    file fieldの値をリセットしないように	  return $('#user_profile_img').replaceWith($('#user_profile_img').clone()); を削除/インデントを修正

--- app/assets/javascripts/cropit.js
+++ app/assets/javascripts/cropit.js
@@ -1,24 +1,22 @@
 $(document).on('turbolinks:load', function() {
-	$('#image-cropper').cropit();
+  $('#image-cropper').cropit()
 
-	// When user clicks select image button,
-	// open select file dialog programmatically
-	$('.select-image-btn').click(function() {
-	  $('.cropit-image-input').click();
-	});
+  // When user clicks select image button,
+  // open select file dialog programmatically
+  $('.select-image-btn').click(function() {
+    $('.cropit-image-input').click
+  })
 
-	// Handle rotation
-	$('.rotate-cw-btn').click(function() {
-	  $('#image-cropper').cropit('rotateCW');
-	});
-	$('.rotate-ccw-btn').click(function() {
-	  $('#image-cropper').cropit('rotateCCW');
-	});
+  // Handle rotation
+  $('.rotate-cw-btn').click(function() {
+    $('#image-cropper').cropit('rotateCW')
+  })
+  $('.rotate-ccw-btn').click(function() {
+    $('#image-cropper').cropit('rotateCCW')
+  })
 
-	$('#user_form').on('submit', function() {
-	  var imageData;
-	  imageData = $('#image-cropper').cropit('export');
-	  $('#user_profile_img_data_uri').val(imageData);
-	  return $('#user_profile_img').replaceWith($('#user_profile_img').clone());
-	});
-});
+  $('#user_form').on('submit', function() {
+    var imageData = $('#image-cropper').cropit('export')
+    $('#user_profile_img_data_uri').val(imageData)
+  })
+})
