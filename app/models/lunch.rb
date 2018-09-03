17b4e31c4a51f6fb138d77f4ec5f7ca21f9be0ae
佐々木勝春 <sasaki-katsuharu@kcgrp.jp>
2018-09-04 00:50:11 +0900

    マッチング不成立メール送信用のメソッドを定義

--- app/models/lunch.rb
+++ app/models/lunch.rb
@@ -3,18 +3,13 @@ class Lunch < ApplicationRecord
     return Lunch.where(lunch_date: Date.today).where(category_id: category_id).where(canceled_at: nil).pluck(:user_id)
   end
 
-  # def self.send_success_email(lunches)
-  #   # 参加ユーザーのメアドの配列を取得
-  #   email_lists = User.where(id: lunches.pluck(:user_id)).pluck(:email)
-  #   # メール一斉位送信
-  #   if UserMailer.matching_success(email_lists).deliver_now!
-  #     # sent_atカラムを更新
-  #     lunches.update_all(sent_at: DateTime.now)
-  #   end
-  # end
-
   def self.send_success_email(email_lists)
     # メール一斉位送信
     UserMailer.matching_success(email_lists).deliver_now!
+  end
+
+  def self.send_fail_email(email_lists)
+    # メール一斉位送信
+    UserMailer.matching_fail(email_lists).deliver_now!
   end
 end
