ab99144ba6dde62c977d44039f59e23caadf9ecc
katsuharu <traveler.18.challenge@gmail.com>
2018-09-09 17:14:34 +0900

    [mod]サンプルユーザーとサンプルランチ、サンプルチャットデータを作成しないように実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,18 +28,18 @@ end
 #   Lunch.create(user_id: info[0], lunch_date: Date.today, category_id: info[2])
 # end
 
-CSV.foreach('db/user_thousand.csv') do |info|
-  User.create(id: info[0], name: info[1], email: info[2], password: info[3], password_confirmation: info[3], department_name: info[4], slack_id: info[5], activated: info[6], activated_at: info[7])
-end
+# CSV.foreach('db/user_thousand.csv') do |info|
+#   User.create(id: info[0], name: info[1], email: info[2], password: info[3], password_confirmation: info[3], department_name: info[4], slack_id: info[5], activated: info[6], activated_at: info[7])
+# end
 
 # CSV.foreach('db/thousand_lunches.csv') do |info|
 #   Lunch.create(user_id: info[0], lunch_date: Date.today, category_id: info[1])
 # end
 
-CSV.foreach('db/make_pair_lunches.csv') do |info|
-  Lunch.create(user_id: info[0], lunch_date: Date.today, category_id: info[1], pair_id: info[2], sent_at: DateTime.now)
-end
+# CSV.foreach('db/make_pair_lunches.csv') do |info|
+#   Lunch.create(user_id: info[0], lunch_date: Date.today, category_id: info[1], pair_id: info[2], sent_at: DateTime.now)
+# end
 
-CSV.foreach('db/all_pair_one_chat.csv') do |info|
-  Chat.create(text: 'a' ,user_id: info[0], pair_id: info[1], lunch_date: Date.today)
-end
+# CSV.foreach('db/all_pair_one_chat.csv') do |info|
+#   Chat.create(text: 'a' ,user_id: info[0], pair_id: info[1], lunch_date: Date.today)
+# end
