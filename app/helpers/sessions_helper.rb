15ec920e83cb74742bac718f4e742f482ca98fee
佐々木勝春 <sasaki-katsuharu@kcgrp.jp>
2018-09-10 23:01:19 +0900

    [mod]userが取得できた場合にlog_inメソッドに引数として渡すように修正

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,9 +23,11 @@ module SessionsHelper
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id:user_id)
       # if user && user.authenticated?(:remember,cookies[:remember_token])
-        log_in user
+      # Userモデルが存在する場合
+      if user.present?
+        log_in(user)
         @current_user = user
-      # end
+      end
     end
   end
 
