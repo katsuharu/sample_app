81ee4145d8c95df9d25bf64a8464625eb2a8f49c
katsuharu <traveler.18.challenge@gmail.com>
2018-09-17 23:27:11 +0900

    [mod]インデントを変更/既に認証済みの場合の処理を追加

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,14 +1,19 @@
 class AccountActivationsController < ApplicationController
-	def edit
-	    user = User.find_by(email: params[:email])
-	    if user && !user.activated? && user.authenticated?(:activation, params[:id])
-	      user.activate
-	      log_in user
-	      flash[:success] = "アカウントが有効化されました。"
-	      redirect_to root_url
-	    else
-	      flash[:danger] = "このリンクは無効です。"
-	      redirect_to root_url
-	    end
-	end
+  def edit
+    user = User.find_by(email: params[:email])
+    # ユーザーが存在しかつ非有効の状態かつの場合
+    if user.present? && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.activate
+      log_in user
+      flash[:success] = "アカウントが有効化されました。"
+      redirect_to root_url
+    # 既に有効化されている場合
+    elsif user.activated? && user.authenticated?(:activation, params[:id])
+      flash[:info] = "アカウントは既に認証済みです。"
+      redirect_to root_url
+    else
+      flash[:danger] = "このリンクは無効です。"
+      redirect_to root_url
+    end
+  end
 end
