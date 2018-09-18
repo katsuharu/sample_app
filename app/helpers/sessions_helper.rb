46ba5641666e072ead6bb5d3a4b8879831d91c81
katsuharu <traveler.18.challenge@gmail.com>
2018-09-18 23:56:59 +0900

    [mod]cookiesのremember_tokenを使ってユーザーの認証をテストするように修正

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,21 +16,20 @@ module SessionsHelper
     user == current_user
   end
 
-  #記憶トークンcookeiに対応するユーザーを返す
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id:user_id)
+      @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id:user_id)
-      # if user && user.authenticated?(:remember,cookies[:remember_token])
-      # Userモデルが存在する場合
-      if user.present?
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember,cookies[:remember_token])
         log_in(user)
         @current_user = user
       end
     end
   end
 
+  # ユーザーがログインしていればtrue、そうでなければfalseを返すメソッド
   def logged_in?
     !current_user.nil?
   end
