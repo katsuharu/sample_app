e0260235637d1ce3a5859a2d403f2019272f4f90
佐々木勝春 <sasaki-katsuharu@kcgrp.jp>
2018-09-20 00:43:53 +0900

    [mod]対象のカテゴリーに引数で渡したユーザ以外のユーザがエントリーしている場合にtrue,そうでない場合にfalseを返す関数を定義

--- app/models/lunch.rb
+++ app/models/lunch.rb
@@ -10,6 +10,18 @@ class Lunch < ApplicationRecord
     end
   end
 
+  # 引数のユーザー以外のユーザーが対象のカテゴリーにエントリー済みの場合にtrueを返す関数
+  def self.is_others(current_user_id, category_id)
+    # 12:30以前の場合
+    if DateTime.now.strftime('%H:%M:%S') < "12:30:00"
+      # 今日の日付の自分以外のユーザーがエントリーしている対象のカテゴリーのLunchモデルが存在する場合にtrue、そうでない場合にfalseを返す
+      Lunch.where(lunch_date: Date.today).where(category_id: category_id).where(canceled_at: nil).where.not(user_id: current_user_id).present?
+    else
+      # 明日の日付の自分以外のユーザーがエントリーしている対象のカテゴリーのLunchモデルが存在する場合にtrue、そうでない場合にfalseを返す
+      Lunch.where(lunch_date: Date.today).where(category_id: category_id).where(canceled_at: nil).where.not(user_id: current_user_id).present?
+    end
+  end
+
   def self.send_success_email(email_lists)
     # メール一斉位送信
     UserMailer.matching_success(email_lists).deliver_now!
