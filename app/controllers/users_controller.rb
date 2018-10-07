467ed93090ab3a8e1abb839dfcf9f8fca0a14f15
佐々木勝春 <traveler.18.challenge@gmail.com>
2018-10-07 16:40:29 +0900

    [mod]表示するランチカードをテーマランチに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,86 +29,90 @@ class UsersController < ApplicationController
         @my_lunch = Lunch.where(user_id: current_user.id).where(lunch_date: Date.tomorrow).where.not(category_id: nil).find_by(canceled_at: nil)
       end
 
-      #######
-      # # ランチカードの配列。初期値としてオールジャンルカテゴリーを代入
+      
+      ########## TOPタイムライン #########
+      # # 投稿フォームのカテゴリーセレクト用のハッシュを定義
+      # @tw_selects = {"オールジャンル" => 43}
+      ##################
+      
+      ########## ユーザーの登録したカテゴリー ############
+      # # # ランチカードの配列。初期値としてオールジャンルカテゴリーを代入
+      # # @cards = [{category_id: 43,
+      # #           category_name: 'オールジャンル',
+      # #           users: User.where(id: Lunch.get_entry_user_ids(43)), # オールジャンルにエントリー中のUserモデルの配列
+      # #           can_entry: true
+      # #           }]
       # @cards = [{category_id: 43,
       #           category_name: 'オールジャンル',
-      #           users: User.where(id: Lunch.get_entry_user_ids(43)), # オールジャンルにエントリー中のUserモデルの配列
+      #           is_others: Lunch.is_others(current_user.id, 43), # 他のユーザーが参加していればtrue、そうでなければfalse
       #           can_entry: true
       #           }]
-      @cards = [{category_id: 43,
-                category_name: 'オールジャンル',
-                is_others: Lunch.is_others(current_user.id, 43), # 他のユーザーが参加していればtrue、そうでなければfalse
-                can_entry: true
-                }]
-      ######
-      # 投稿フォームのカテゴリーセレクト用のハッシュを定義
-      @tw_selects = {"オールジャンル" => 43}
-      # ログインユーザーが登録したカテゴリーの名前の配列を取得
-      user_cards = UserHobby.where(user_id: current_user.id).pluck(:hobby_name)
-      # 登録したカテゴリーの数分繰り返す
-      user_cards.each do |u_card|
-        category = Category.find_by(name: u_card)
-        # カテゴリーが存在する場合
-        if category.present?
-          # カテゴリーidを取得
-          category_id = category.id
-          # このカテゴリーにエントリー中の場合
-          if @my_lunch.present? && category_id == @my_lunch.category_id
-            # 自分が登録したカテゴリーのなかで登録ユーザー数が3人以上場合
-            if UserHobby.where(hobby_name: u_card).count > 2
-              ###########
-              # ランチカードで一番最初に表示されるように配列の先頭にハッシュを追加
-              # @cards.unshift(
-              # {category_id: category_id, # カテゴリーID
-              # category_name: u_card, # 3名以上のユーザーが登録したカテゴリーのカテゴリ名
-              # users: User.where(id: Lunch.get_entry_user_ids(category_id)), # 各カテゴリーにエントリー中のUserモデルの配列
-              # can_entry: true
-              # })
-              @cards.unshift(
-              {category_id: category_id, # カテゴリーID
-              category_name: u_card, # 3名以上のユーザーが登録したカテゴリーのカテゴリ名
-              is_others: Lunch.is_others(current_user.id, category_id),
-              can_entry: true
-              })
-              ######
-            else
-              # ランチカードで一番最初に表示されるように配列の先頭にハッシュを追加
-              @cards.unshift(
-              {category_id: category_id, # カテゴリーID
-              category_name: u_card, # 3名以上のユーザーが登録したカテゴリーのカテゴリ名
-              # users: User.where(id: Lunch.get_entry_user_ids(category_id)), # 各カテゴリーにエントリー中のUserモデルの配列
-              is_others: Lunch.is_others(current_user.id, category_id),
-              can_entry: false
-              })
-            end
-          else
-            if UserHobby.where(hobby_name: u_card).count > 2
-              # 配列の末尾にハッシュを追加
-              @cards.push(
-              {category_id: category_id, # カテゴリーID
-              category_name: u_card, # 3名以上のユーザーが登録したカテゴリーのカテゴリ名
-              # users: User.where(id: Lunch.get_entry_user_ids(category_id)), # 各カテゴリーにエントリー中のUserモデルの配列
-              is_others: Lunch.is_others(current_user.id, category_id),
-              can_entry: true
-              })
-            else
-              # 配列の末尾にハッシュを追加
-              @cards.push(
-              {category_id: category_id, # カテゴリーID
-              category_name: u_card, # 3名以上のユーザーが登録したカテゴリーのカテゴリ名
-              # users: User.where(id: Lunch.get_entry_user_ids(category_id)), # 各カテゴリーにエントリー中のUserモデルの配列
-              is_others: Lunch.is_others(current_user.id, category_id),
-              can_entry: false
-              })
-            end
-          end
-          # カテゴリーハッシュに追加
-          @tw_selects[u_card] = category_id
-        end
-      end
+      # # ログインユーザーが登録したカテゴリーの名前の配列を取得
+      # user_cards = UserHobby.where(user_id: current_user.id).pluck(:hobby_name)
+      # # 登録したカテゴリーの数分繰り返す
+      # user_cards.each do |u_card|
+      #   category = Category.find_by(name: u_card)
+      #   # カテゴリーが存在する場合
+      #   if category.present?
+      #     # カテゴリーidを取得
+      #     category_id = category.id
+      #     # このカテゴリーにエントリー中の場合
+      #     if @my_lunch.present? && category_id == @my_lunch.category_id
+      #       # 自分が登録したカテゴリーのなかで登録ユーザー数が3人以上場合
+      #       if UserHobby.where(hobby_name: u_card).count > 2
+      #         ###########
+      #         # ランチカードで一番最初に表示されるように配列の先頭にハッシュを追加
+      #         # @cards.unshift(
+      #         # {category_id: category_id, # カテゴリーID
+      #         # category_name: u_card, # 3名以上のユーザーが登録したカテゴリーのカテゴリ名
+      #         # users: User.where(id: Lunch.get_entry_user_ids(category_id)), # 各カテゴリーにエントリー中のUserモデルの配列
+      #         # can_entry: true
+      #         # })
+      #         @cards.unshift(
+      #         {category_id: category_id, # カテゴリーID
+      #         category_name: u_card, # 3名以上のユーザーが登録したカテゴリーのカテゴリ名
+      #         is_others: Lunch.is_others(current_user.id, category_id),
+      #         can_entry: true
+      #         })
+      #         ######
+      #       else
+      #         # ランチカードで一番最初に表示されるように配列の先頭にハッシュを追加
+      #         @cards.unshift(
+      #         {category_id: category_id, # カテゴリーID
+      #         category_name: u_card, # 3名以上のユーザーが登録したカテゴリーのカテゴリ名
+      #         # users: User.where(id: Lunch.get_entry_user_ids(category_id)), # 各カテゴリーにエントリー中のUserモデルの配列
+      #         is_others: Lunch.is_others(current_user.id, category_id),
+      #         can_entry: false
+      #         })
+      #       end
+      #     else
+      #       if UserHobby.where(hobby_name: u_card).count > 2
+      #         # 配列の末尾にハッシュを追加
+      #         @cards.push(
+      #         {category_id: category_id, # カテゴリーID
+      #         category_name: u_card, # 3名以上のユーザーが登録したカテゴリーのカテゴリ名
+      #         # users: User.where(id: Lunch.get_entry_user_ids(category_id)), # 各カテゴリーにエントリー中のUserモデルの配列
+      #         is_others: Lunch.is_others(current_user.id, category_id),
+      #         can_entry: true
+      #         })
+      #       else
+      #         # 配列の末尾にハッシュを追加
+      #         @cards.push(
+      #         {category_id: category_id, # カテゴリーID
+      #         category_name: u_card, # 3名以上のユーザーが登録したカテゴリーのカテゴリ名
+      #         # users: User.where(id: Lunch.get_entry_user_ids(category_id)), # 各カテゴリーにエントリー中のUserモデルの配列
+      #         is_others: Lunch.is_others(current_user.id, category_id),
+      #         can_entry: false
+      #         })
+      #       end
+      #     end
+      #     # カテゴリーハッシュに追加
+      #     @tw_selects[u_card] = category_id
+      #   end
+      # end
+      ######################
 
-      @user = current_user
+      # @user = current_user
       # @user.update_attribute(:logined_at, DateTime.now)
 
       # # Timelineに投稿された場合
@@ -142,6 +146,20 @@ class UsersController < ApplicationController
       #     format.json { @new_tweets }
       #   end
       # end
+
+      ########## テーマランチ ############
+      # ランチカード表示用の配列
+      @cards = []
+      # ランチカード用にDailyLunchを取得
+      daily_lunches = DailyLunch.get_card
+      daily_lunches.each do |daily_lunch|
+        @cards.push({category_id: daily_lunch.category_id,
+          category_name: Category.find(daily_lunch.category_id).name,
+          is_others: Lunch.is_others(current_user.id, daily_lunch.category_id), # 他ユーザーが参加していればtrue
+          can_entry: true
+        })
+      end
+      ######################
 
     # 未ログインの場合
     else
--- app/models/daily_lunch.rb
+++ app/models/daily_lunch.rb
@@ -1,4 +1,17 @@
 class DailyLunch < ApplicationRecord
+  # 12:30以前の場合は本日の日付の、以降の場合は明日の日付のDailyLunchを取得する関数
+  def self.get_card
+    # 12:30以前の場合
+    if DateTime.now.strftime('%H:%M:%S') < "12:30:00"
+      # dateカラムの値が本日のレコードを取得
+      daily_lunches = DailyLunch.where(date: Date.today).where(deleted_at: nil)
+    else
+      # 明日の日付のレコードを取得
+      daily_lunches = DailyLunch.where(date: Date.tomorrow).where(deleted_at: nil)
+    end
+  end
+
+  # 12:30以前の場合は本日以降の、12:30以降の場合は明日以降の日付のDailyLunchを取得する関数
   def self.get_plans
     # 12:30以前の場合
     if DateTime.now.strftime('%H:%M:%S') < "12:30:00"
